b9bbbe547c937d6c87f6e9d89a47541b85980326
deng_puhua <dengpuhua@vwiov.com>
2019-11-25 17:35:55 +0800

    add to change

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # reinforce
 ## koa + sequelize + mysql + typescript
-### |--src                              资源目录
-### |---|--app.ts                       入口文件
-### |---|--config                       配置文件
-### |---|---|--token.ts                 token配置
-### |---|---|--whitelist.ts             白名单配置
-### |---|--controllers                  控制层--逻辑层
-### |---|---|--user                     用户相关逻辑
-### |---|--db                           数据库
-### |---|---|--base_table.ts            表基类
-### |---|---|--user.ts                  用户表
-### |---|--public                       公共资源目录
-### |---|--routes                       路由目录
-### |---|--views                        中间页面层
-### |--dist                             打包文件
-### |--bin                              启动文件
+#### 目前没有区分开发和生产环境
+#### 
+#### 启动开发环境 npm run dev
+#### 
+#### 生产环境
+#### --| 1、docker：Dockerfile配置
+#### --| 2、pm2：npm run build 之后再 npm run prd
+#### --| 3、node：npm run build 之后再 npm run dist
+#### 
+##### |--src                              资源目录
+##### |---|--app.ts                       入口文件
+##### |---|--config                       配置文件
+##### |---|---|--token.ts                 token配置
+##### |---|---|--whitelist.ts             白名单配置
+##### |---|--controllers                  控制层--逻辑层
+##### |---|---|--user                     用户相关逻辑
+##### |---|--db                           数据库
+##### |---|---|--base_table.ts            表基类
+##### |---|---|--user.ts                  用户表
+##### |---|--public                       公共资源目录
+##### |---|--routes                       路由目录
+##### |---|--views                        中间页面层
+##### |--dist                             打包文件
+##### |--bin                              启动文件
--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,14 @@ import koaJwt from 'koa-jwt' // Json Web Tokens 用于token认证
 import jwt from 'jwt-simple' // 用于操作jwt产生的token，列如间token中的信息提取
 import koaStatic from 'koa-static' // 加载本地文件
 import http from 'http'
+import sequelize from '~/db/test' // test数据库
+import * as models from '~/db/test_table' // 模型，test数据库下的表
 
-import routers from './routes'
-import tokenConfig from './config/token'
+import * as routers from './routes' // 路由
+import tokenConfig from './config/token' // token配置
+
+// sequelize添加模型
+sequelize.addModels(Object.values(models))
 
 // middlewares
 app.use(cors()) // 此处跨域有问题，应该传入参数，不应该在下面直接设置响应头
@@ -25,59 +30,60 @@ app.use(views(__dirname + '/views', {
   extension: 'pug'
 }))
 
-// app.use(async (ctx, next) => {
-//   // origin
-//   ctx.set('Access-Control-Allow-Origin', '*')
-//   ctx.set('Access-Control-Allow-Credentials', 'true')
-//   await next().then( () => {
-//     if (ctx.status === 404) {
-//       ctx.body = {
-//         code: -1,
-//         msg: '接口不存在'
-//       }
-//     }
-//     // 请求成功返回200状态码，数据正确性通过body的code区分
-//     ctx.status = 200
-//   }).catch( (err) => {
-//     // token error
-//     if (err.status === 401) {
-//       ctx.body = {
-//         code: -11,
-//         msg: '未登录'
-//       }
-//     } else {
-//       throw err
-//     }
-//   })
-// })
+app.use(async (ctx, next) => {
+  // origin
+  ctx.set('Access-Control-Allow-Origin', '*')
+  ctx.set('Access-Control-Allow-Credentials', 'true')
+  await next().then( () => {
+    if (ctx.status === 404) {
+      ctx.body = {
+        code: -1,
+        msg: '接口不存在'
+      }
+    }
+    // 请求成功返回200状态码，数据正确性通过body的code区分
+    ctx.status = 200
+  }).catch( (err) => {
+    // token error
+    if (err.status === 401) {
+      ctx.body = {
+        code: -11,
+        msg: '未登录'
+      }
+    } else {
+      throw err
+    }
+  })
+})
 
-// const widthPath = /^\/api\/user\/(login|register|forget|test)/
+// 白名单配置
+const widthPath = /^\/api\/user\/(login|register|forget|test)/
 
 // valid
-// app.use(koaJwt({secret: tokenConfig.secret}).unless({
-//   path: [widthPath]
-// }))
+app.use(koaJwt({secret: tokenConfig.secret}).unless({
+  path: [widthPath]
+}))
 
 // valid token
-// app.use(async (ctx, next) => {
-//   if (!widthPath.test(ctx.url)) {
-//     const deToken = jwt.decode(ctx.request.header.authorization.replace('Bearer ', ''), tokenConfig.secret)
-//     const expTime = deToken.exp - new Date().getTime()
-//     if (expTime < 0) {
-//       ctx.body = {
-//         code: -17,
-//         msg: '登录已失效'
-//       }
-//     } else {
-//       await next()
-//     }
-//   } else {
-//     await next()
-//   }
-// })
+app.use(async (ctx, next) => {
+  if (!widthPath.test(ctx.url)) {
+    const deToken = jwt.decode(ctx.request.header.authorization.replace('Bearer ', ''), tokenConfig.secret)
+    const expTime = deToken.exp - new Date().getTime()
+    if (expTime < 0) {
+      ctx.body = {
+        code: -17,
+        msg: '登录已失效'
+      }
+    } else {
+      await next()
+    }
+  } else {
+    await next()
+  }
+})
 
 // routes
-routers.forEach((route) => {
+Object.values(routers).forEach((route) => {
   app.use(route.routes())
 })
 
